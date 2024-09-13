f41126971aff4eb5fac0c63eba054d5bff246113
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-09-13 14:56:47 +0300

    add global variables for colors in css files

--- react-app4/lib/mealsDataLoad.js
+++ react-app4/lib/mealsDataLoad.js
@@ -7,7 +7,7 @@ const db = sql('meals.db')
 
 export async function getMeals() {
     //arbitrary delay to simulate database response
-    await new Promise(resolve => setTimeout(resolve, 5000))
+    // await new Promise(resolve => setTimeout(resolve, 5000))
 
     // throw new Error('Meals failed');
     return db.prepare('SELECT * FROM meals').all();
